544c3553a38bd039c7e071d77b366800ea5e38d0
Huzaifa Nadeem <huzaifanadeemkamboh@gmail.com>
2024-12-12 16:38:46 +0500

    Create phpIntroAssignment.php
    PHP 1st Basic Assignment

---
+++ phpIntroAssignment.php
@@ -0,0 +1,22 @@
+<?php
+$myName = "Huzaifa Nadeem";
+$myAge = 21;
+$myFavColor = "White";
+
+echo "<h3>Hello, My name is $myName. I am $myAge years old. My favourite color is $myFavColor.<h3> <br>";
+
+$num1 = 10;
+$num2 = 5;
+$addition = $num1 + $num2;
+$subtraction = $num1 - $num2;
+$multiplication = $num1 * $num2;
+$division = $num1 / $num2;
+
+echo "<br><br>";
+echo "The Addition, subtraction, multiplication and division of numbers 10 & 5 is shown below: <br>";
+echo "Addition = $addition <br>";
+echo "Subtraction = $subtraction <br>";
+echo "Multiplication = $multiplication <br>";
+echo "Division = $division <br>";
+ 
+
